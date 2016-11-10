84948f07806853435ea14c93a5f255bf05b25243
cfx.404 <C-F-4@users.noreply.github.com>
2016-11-10 11:36:18 +0530

    Update list_0.cpp
    Insertion in Linked List

--- RawCode/list_0.cpp
+++ RawCode/list_0.cpp
@@ -1,3 +1,4 @@
+
 /*
  * Arrays - fixed size
  *        - better cache locality
@@ -14,6 +15,8 @@
 
 using std::cin;
 using std::cout;
+using std::clog;
+using std::cerr;
 using std::endl;
 
 class Node {
@@ -21,17 +24,11 @@ private:
   int data;
   Node* next;
 public:
-  Node() {
-    data = 0;
-    next = NULL;
+  Node():data(0), next(nullptr) {
   }
-  Node(int d) {
-    data = d;
-    next = NULL;
+  Node(int d):data(d), next(nullptr) {
   }
-  Node(int d, Node* n) {
-    data = d;
-    next = n;
+  Node(int d, Node* n):data(d), next(n) {
   }
 
   int GetData() {
@@ -52,15 +49,86 @@ public:
 class LinkedList {
 private:
   Node* head;
+  Node* last;
 public:
-  LinkedList() {
-    head = NULL;
+  LinkedList():head(nullptr), last(nullptr) {
+  }
+
+  Node* GetHead() {
+    return head;
+  }
+
+  Node* GetLast() {
+    return last;
   }
 
   void InsertBeg(int d) {
     Node* newNode = new Node(d);
     newNode->SetNext(head);
     head = newNode;
+    if(last == nullptr) {
+      last = head;
+    }
+    return;
+  }
+
+  void InsertAfter(int d, Node* prev) {
+    if(prev == nullptr || head == nullptr || last == nullptr) {
+      clog << "Modifying the head" << endl;
+      head = new Node(d);
+      last = head;
+      return;
+    }
+    else {
+      // check if prev is valid
+      Node* tmp = head;
+      while(tmp != prev && tmp != nullptr) {
+        tmp = tmp->GetNext();
+      }
+      if(tmp != prev) {
+        cerr << "Previous Pointer not found" << endl;
+        return;
+      }
+      Node* newNode = new Node(d, prev->GetNext());
+      prev->SetNext(newNode);
+      if(prev == last) {
+        last = last->GetNext();
+      }
+      return;
+    }
+  }
+
+  void InsertEnd(int d) {
+    Node* newNode = new Node(d);
+    if(last == nullptr) {
+      clog << "Modifying the head" << endl;
+      head = newNode;
+      last = newNode;
+      return;
+    }
+    else {
+      last->SetNext(newNode);
+      last = last->GetNext();
+      return;
+    }
+  }
+
+  void append(int d) {
+    Node* newNode = new Node(d);
+    if(head == nullptr) {
+      clog << "Modifying the head" << endl;
+      head = newNode;
+      last = newNode;
+      return;
+    }
+    else {
+      Node* tmp = head;
+      while(tmp->GetNext()) {
+        tmp = tmp->GetNext();
+      }
+      tmp->SetNext(newNode);
+      last = tmp->GetNext();
+    }
   }
 
   void print() {
@@ -83,6 +151,12 @@ int main(void) {
     cin >> tmp;
     my_list.InsertBeg(tmp);
   }
+  my_list.print();
+  my_list.InsertAfter(1, my_list.GetHead());
+  my_list.print();
+  my_list.InsertEnd(7);
+  my_list.print();
+  my_list.append(8);
   my_list.print();
   return 0;
 }
